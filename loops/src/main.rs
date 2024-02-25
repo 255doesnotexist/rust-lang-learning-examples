11f1897a60a0fb434505531d05fed416b2f45ff3
255doesnotexist <chenyejin2004@stumail.ysu.edu.cn>
2024-02-25 16:08:18 +0800

    loops and array and while and for

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,7 +1,30 @@
 fn main() {
     println!("Hello, world!");
 
+    let mut cnt = 1;
+
     loop{
-        println!("loop print");
+        if cnt >= 3 {
+            break;
+        }
+        println!("{}", cnt);
+        cnt += 1
+    }
+    println!("cnt {}", cnt);
+
+    let loop_ret = loop{
+        cnt -= 1;
+        break cnt;
+    };
+    println!("loopret {}", loop_ret);
+
+    let while_ret = while cnt >= -3 {
+        cnt -= 4;
+    };
+    // println!("{}", while_ret); while always return a () so cannot be formatted & printed
+
+    let arr: [i32; 6] = [233, 244, 255, 266, 277, 288];
+    for i in arr.iter() {
+        println!("arr {}", i);
     }
 }
