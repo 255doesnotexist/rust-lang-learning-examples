a622d7bf16ef39158f9d2254761f2475c469ddcb
255doesnotexist <chenyejin2004@stumail.ysu.edu.cn>
2024-02-25 17:55:18 +0800

    make slice and for iter enumerate

--- slice/src/main.rs
+++ slice/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
     println!("Hello, world!");
+    
+    let s1 = "unsliced";
+    let s2 = &s1[2..]; // [left, right) -> left..right
+    println!("{} {}", s1, s2);
+    // unsliced sliced
+
+    let a1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    for (i, ai) in a1[3..6].iter().enumerate() {
+        println!("a1[{}] = {}", i, ai);
+    }
+    // 3, 4, 5
+
+    // slice won't take ownership
 }
