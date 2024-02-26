22e4fb6feec04eb801e70cafa4f598f3ba0743f6
255doesnotexist <chenyejin2004@stumail.ysu.edu.cn>
2024-02-26 17:49:08 +0800

    and association function

--- struct_exp/src/main.rs
+++ struct_exp/src/main.rs
@@ -14,6 +14,14 @@ impl User {
     fn older(&self, user2: &User) -> bool {
         self.age > user2.age
     }
+
+    // association function (usually constructor) 
+    fn create(name: String, age: i32) -> User {
+        User {
+            name, // field have same name with the variable so 可以简写 
+            age,
+        }
+    }
 }
 
 fn main() {
@@ -21,7 +29,7 @@ fn main() {
 
     let u1 = User {
         name: String::from("user1"),
-        age: 19
+        age: 19,
     };
 
     println!("u1 name: {} age: {}", u1.name, u1.age);
@@ -31,8 +39,11 @@ fn main() {
 
     let u2 = User {
         name: String::from("user2"),
-        age: 18
+        age: 18,
     };
 
     println!("{} is {} than {}", u1.name, if u1.older(&u2) {"older"} else {"younger"}, u2.name);
+
+    let u3 = User::create("user3".to_string(), 21);
+    println!("{:?}", u3);
 }
