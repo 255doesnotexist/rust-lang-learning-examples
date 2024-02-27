129f32e07731c48fa22a6e709d08b8d3797eaed3
255doesnotexist <chenyejin2004@stumail.ysu.edu.cn>
2024-02-27 13:24:30 +0800

    make route impl inner enum

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -4,22 +4,24 @@ enum IpAddrKind {
     V6(String),
 }
 
+impl IpAddrKind{    
+    fn route(&self) {
+        match self {
+            IpAddrKind::V4(a, b, c, d) => {
+                println!("route ipv4 {}.{}.{}.{}", a, b, c, d);
+            }
+            IpAddrKind::V6(addr) => {
+                println!("route ipv6 {}", addr);
+            }
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 
     let googledns_v4 = IpAddrKind::V4(8, 8, 8, 8);
     let googledns_v6 = IpAddrKind::V6("2001:4860:4860::8888".to_string());
 
-    route(googledns_v4); route(googledns_v6);
-}
-
-fn route(ipaddr: IpAddrKind) {
-    match ipaddr {
-        IpAddrKind::V4(a, b, c, d) => {
-            println!("route ipv4 {}.{}.{}.{}", a, b, c, d);
-        }
-        IpAddrKind::V6(addr) => {
-            println!("route ipv6 {}", addr);
-        }
-    }
+    googledns_v4.route(); googledns_v6.route();
 }
