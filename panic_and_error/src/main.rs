64b966c8fa3b2dcae68ec5b22cdac15ac4c62dd7
255doesnotexist <chenyejin2004@stumail.ysu.edu.cn>
2024-03-01 00:07:25 +0800

    make comment

--- panic_and_error/src/main.rs
+++ panic_and_error/src/main.rs
@@ -1,4 +1,5 @@
 fn main() {
     println!("Hello, world!");
     panic!("crash and burn");
+    // make panic = abort could let rust abort calling stack immediately instead of unwinding the stack 
 }
