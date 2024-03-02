096af2843d6bc1ec0d569f310c11d0db8ed91497
255doesnotexist <chenyejin2004@stumail.ysu.edu.cn>
2024-03-02 23:01:29 +0800

    implemented the largest function

--- largest_fn/src/main.rs
+++ largest_fn/src/main.rs
@@ -1,3 +1,17 @@
+use std::cmp::max;
+
 fn main() {
     println!("Hello, world!");
+
+    let nums = [1, 3, 5, 7, 9, -1];
+    println!("the largest num in list is {}", largest(&nums));
+}
+
+// input ref of i32 sliced list and return the max value 
+fn largest(list: &[i32]) -> i32 {
+    let mut ret: i32 = i32::MIN;
+    for &i in list { // &i will auto deref so *i* will be i32. if you put "for i in list" here *i* will be i32 
+        ret = max(ret, i);
+    }
+    ret
 }
