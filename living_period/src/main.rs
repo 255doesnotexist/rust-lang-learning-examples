c8e055d8b9b01ac3fc24f4c3e19fd5d9241edb08
255doesnotexist <chenyejin2004@stumail.ysu.edu.cn>
2024-03-03 12:17:50 +0800

    right code here

--- living_period/src/main.rs
+++ living_period/src/main.rs
@@ -1,12 +1,21 @@
 fn main() {
     println!("Hello, world!");
 
+    // let a = "A".to_string();
+    // let c;
+    // {
+    //     let b = "B".to_string();
+    //     c = longest(&a, &b);
+    // }
+    // println!("{}", c);
+    // borrowed value does not live long enough
+
     let a = "A".to_string();
+    let b = "B".to_string();
     let c;
     {
-        let b = "B".to_string();
         c = longest(&a, &b);
-    }
+    } // that's right 
     println!("{}", c);
 }
 
