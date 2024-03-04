6b7ef15e11758bd8b6e2c67b6d52d94680f90807
255doesnotexist <chenyejin2004@stumail.ysu.edu.cn>
2024-03-04 21:34:25 +0800

    test func and cargo test

---
+++ hello_test/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+#[test]
+fn test() {
+    println!("test function acted");
+
+    assert_eq!(1+1, 2); // passed test 
+    // assert_eq!(1+1, 3); // this will make test failed 
+}
