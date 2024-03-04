0e9dea15cbe005f613d677fe800d8824805c1610
255doesnotexist <chenyejin2004@stumail.ysu.edu.cn>
2024-03-04 21:41:07 +0800

    customized assert message

--- hello_test/src/main.rs
+++ hello_test/src/main.rs
@@ -10,5 +10,7 @@ fn test() {
     // assert_eq!(1+1, 3); // this will make test failed 
     // assert_ne!(1+1, 2); // this will make test failed 
     // panic!("this will make test failed too");
-    // assert!(true or false); // false make test failed 
+    // assert!(true or false); // false make test failed
+
+    // assert-family macros could add one more parameter to output customized message  
 }
