b8996e8ddb4d9b488f8fda02588f54f9c3a79ae1
255doesnotexist <chenyejin2004@stumail.ysu.edu.cn>
2024-03-05 15:38:09 +0800

    struct & impl config

--- minicat/src/main.rs
+++ minicat/src/main.rs
@@ -1,6 +1,6 @@
 use std::env;
 use std::fs::read_to_string;
-use std::process::exit;
+use std::process;
 use std::path::Path;
 use std::io;
 use std::error::Error;
@@ -8,27 +8,31 @@ use std::error::Error;
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().skip(1).collect();
 
-    let contents = reading_contents(&args)?;
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problems happened because: {}", err);
+        process::exit(1);
+    });
 
-    for (i, content) in contents.iter().enumerate() {
+    for (i, content) in config.contents.iter().enumerate() {
         print!("{}", content);
     }
 
     Ok(())
 }
 
-fn reading_contents(args: &Vec<String>) -> Result<Vec<String>, Box<dyn Error> > {
-    let mut contents: Vec<String> = Vec::new();
-    for (i, arg) in args.iter().enumerate() {
-        let path = Path::new(&arg);
-        let content = match read_to_string(path) {
-            Ok(s) => s,
-            Err(_) => {
-                eprintln!("Reading the file {} failed.", arg);
-                exit(1);
-            }
-        };
-        contents.push(content);
-    }
-    contents
+struct Config {
+    contents: Vec<String>
 }
+
+impl Config {
+    fn new(args: &Vec<String>) -> Result<Config, Box<dyn Error> > {
+        let mut contents: Vec<String> = Vec::new();
+        for (i, arg) in args.iter().enumerate() {
+            let path = Path::new(&arg);
+            let content = read_to_string(path)?;
+            contents.push(content);
+        }
+        Ok(Config{ contents })
+    }
+}
+
