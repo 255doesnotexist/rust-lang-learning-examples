88a2d17fbe36d0a2cc19df7ad8feee829a06a7e2
255doesnotexist <chenyejin2004@stumail.ysu.edu.cn>
2024-03-05 16:13:56 +0800

    now concat with crlf switch added (impl with environment varibles)

--- minicat/src/lib.rs
+++ minicat/src/lib.rs
@@ -2,9 +2,11 @@ use std::fs::read_to_string;
 use std::path::Path;
 use std::process;
 use std::error::Error;
+use std::env;
 
 pub struct Config {
-    contents: Vec<String>
+    pub contents: Vec<String>,
+    pub append_with_crlf: bool,
 }
 
 impl Config {
@@ -15,15 +17,24 @@ impl Config {
             let content = read_to_string(path)?;
             contents.push(content);
         }
-        Ok(Config{ contents })
+
+        let append_with_crlf = env::var("CONCAT_APPEND_WITH_CRLF").is_err();
+        Ok(Config{ contents, append_with_crlf })
     }
 }
 
 pub fn run(config: &Config) -> Result<(), Box<dyn Error> > {
-    print!("{}", concat(&config.contents).unwrap_or_else(|err|{
-        eprintln!("Problems when concating: {}", err);
-        process::exit(1);
-    }));
+    if config.append_with_crlf {
+        print!("{}", concat_with_crlf(&config.contents).unwrap_or_else(|err|{
+            eprintln!("Problems when concating: {}", err);
+            process::exit(1);
+        }));
+    } else {
+        print!("{}", concat(&config.contents).unwrap_or_else(|err|{
+            eprintln!("Problems when concating: {}", err);
+            process::exit(1);
+        }));
+    }
 
     Ok(())
 }
@@ -33,6 +44,19 @@ pub fn concat(v: &Vec<String>) -> Result<String, Box<dyn Error> > {
 
     for (i, content) in v.iter().enumerate() {
         ret += content;
+    }
+
+    Ok(ret)
+}
+
+pub fn concat_with_crlf(v: &Vec<String>) -> Result<String, Box<dyn Error> > {
+    let mut ret: String = String::new();
+
+    for (i, content) in v.iter().enumerate() {
+        ret += content;
+        if i != v.len() - 1 {
+            ret += "\n";
+        }
     }
 
     Ok(ret)
@@ -53,8 +77,10 @@ mod tests {
     #[test]
     fn concat_test() -> Result<(), Box<dyn Error> > {
         let result = concat(&vec!["first".to_string(), "second".to_string()]).unwrap();
+        let result_with_crlf = concat_with_crlf(&vec!["first".to_string(), "second".to_string()]).unwrap();
 
         assert_eq!(result, "firstsecond");
+        assert_eq!(result_with_crlf, "first\nsecond");
         Ok(())
     }
 }
