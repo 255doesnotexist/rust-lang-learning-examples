e1365d0b8c6130574d664de9865f1a3333206572
255doesnotexist <chenyejin2004@stumail.ysu.edu.cn>
2024-03-05 16:05:00 +0800

    wrong path and concat test

--- minicat/src/lib.rs
+++ minicat/src/lib.rs
@@ -1,5 +1,6 @@
 use std::fs::read_to_string;
 use std::path::Path;
+use std::process;
 use std::error::Error;
 
 pub struct Config {
@@ -19,8 +20,41 @@ impl Config {
 }
 
 pub fn run(config: &Config) -> Result<(), Box<dyn Error> > {
-    for (i, content) in config.contents.iter().enumerate() {
-        print!("{}", content);
-    }
+    print!("{}", concat(&config.contents).unwrap_or_else(|err|{
+        eprintln!("Problems when concating: {}", err);
+        process::exit(1);
+    }));
+
     Ok(())
+}
+
+pub fn concat(v: &Vec<String>) -> Result<String, Box<dyn Error> > {
+    let mut ret: String = String::new();
+
+    for (i, content) in v.iter().enumerate() {
+        ret += content;
+    }
+
+    Ok(ret)
+}
+
+# [cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn wrong_path() {
+        let cfg = match Config::new(&vec!["\\totally_wrong_path/wontLocateAnyFile".to_string()]) {
+            Ok(s) => Err(s),
+            Err(e) => Ok(e),
+        };
+    }
+
+    #[test]
+    fn concat_test() -> Result<(), Box<dyn Error> > {
+        let result = concat(&vec!["first".to_string(), "second".to_string()]).unwrap();
+
+        assert_eq!(result, "firstsecond");
+        Ok(())
+    }
 }
