cdfeed9c6dd7373dc948b2e0ee0afd70b91a8031
255doesnotexist <chenyejin2004@stumail.ysu.edu.cn>
2024-03-12 23:06:36 +0800

    bad api dont know how to do

--- tauri_hello/src/main.js
+++ tauri_hello/src/main.js
@@ -6,6 +6,8 @@ let greetMsgEl;
 async function greet() {
   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
   greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+  alert(greetMsgEl.textContent);
+  // this alert doesnt work because of "Unhandled Promise Rejection: The `Dialog` module is not enabled. You must enable one of its APIs in the allowlist."
 }
 
 window.addEventListener("DOMContentLoaded", () => {
