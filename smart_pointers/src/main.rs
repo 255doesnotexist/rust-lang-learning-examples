1f1daeee742290288e49b6c0b03e8278e1dea4c0
255doesnotexist <chenyejin2004@stumail.ysu.edu.cn>
2024-03-24 20:56:58 +0800

    recursive list without pointers will cause infinite recurse and compilation failures

--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -1,4 +1,15 @@
+// fn main() {
+//     let b = Box::new(5); // save 5 on heap 
+//     println!("b = {}", b);
+// }
+
+enum List {
+    Cons(i32, List),
+    Nil,
+}
+
+use crate::List::{Cons, Nil};
+
 fn main() {
-    let b = Box::new(5); // save 5 on heap 
-    println!("b = {}", b);
+    let list = Cons(1, Cons(2, Cons(3, Nil)));
 }
