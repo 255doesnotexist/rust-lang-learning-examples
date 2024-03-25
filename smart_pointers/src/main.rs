ef187fb37862a47c3a7b2ac2b0b8412232d2ef59
255doesnotexist <1293883574jcy@gmail.com>
2024-03-25 13:40:46 +0800

    Box and ref

--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -12,6 +12,19 @@ use crate::List::{Cons, Nil};
 
 fn main() {
     let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+
+    let x = 5;
+    let y = &x;
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y); // assert passed so they are equal (with auto deref)
+
+
+    let x = 5;
+    let y = Box::new(x); // also passed so Box could work like native ref 
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
 }
 
 // 评论：这不是unique_ptr吗？？？ 
