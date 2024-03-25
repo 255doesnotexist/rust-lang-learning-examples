add7954648e27247149b7ebae6c4e4920f2a1ce7
255doesnotexist <1293883574jcy@gmail.com>
2024-03-25 13:42:13 +0800

    impl MyBox<T>

--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -28,3 +28,24 @@ fn main() {
 }
 
 // 评论：这不是unique_ptr吗？？？ 
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+// impl the MyBox<T> by taking its ownership 
+
+use std::ops::Deref;
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
