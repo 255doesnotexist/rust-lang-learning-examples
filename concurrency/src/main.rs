bc865ca5cda3f2d91a2e1e16013797941b8ddb09
255doesnotexist <1293883574jcy@gmail.com>
2024-04-07 08:43:19 +0800

    thread_handle.join will wait for the spawned thread to finish

--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -4,7 +4,7 @@ fn main() {
 }
 */
 
-use std::thread;
+/* use std::thread;
 use std::time::Duration;
 
 fn main() {
@@ -19,7 +19,7 @@ fn main() {
         println!("hi number {} from the main thread!", i);
         thread::sleep(Duration::from_millis(1));
     }
-}
+} */
 
 // The spawned thread will run concurrently with the main thread.
 // THe printing line are wholely sperated from each other. It seems atomic? 
@@ -35,3 +35,23 @@ hi number 4 from the main thread!
 hi number 4 from the spawned thread! */
 
 // After the end of the main thread, the spawned thread will be terminated.
+
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    let handle = thread::spawn(|| {
+        for i in 1..10 {
+            println!("hi number {} from the spawned thread!", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for i in 1..5 {
+        println!("hi number {} from the main thread!", i);
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    handle.join().unwrap();
+    // this will wait for the spawned thread to finish 
+}
