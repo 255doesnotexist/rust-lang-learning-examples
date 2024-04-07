b792bc4475fc6f916fb45eafb42aa2593258d8d7
255doesnotexist <1293883574jcy@gmail.com>
2024-04-07 08:41:10 +0800

    the spawned thread will be terminated after the end of main thread

--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -1,3 +1,37 @@
+/* 
 fn main() {
     println!("Hello, concurrency world!");
 }
+*/
+
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    thread::spawn(|| {
+        for i in 1..10 {
+            println!("hi number {} from the spawned thread!", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for i in 1..5 {
+        println!("hi number {} from the main thread!", i);
+        thread::sleep(Duration::from_millis(1));
+    }
+}
+
+// The spawned thread will run concurrently with the main thread.
+// THe printing line are wholely sperated from each other. It seems atomic? 
+
+// output: 
+/* hi number 1 from the main thread!
+hi number 1 from the spawned thread!
+hi number 2 from the main thread!
+hi number 2 from the spawned thread!
+hi number 3 from the main thread!
+hi number 3 from the spawned thread!
+hi number 4 from the main thread!
+hi number 4 from the spawned thread! */
+
+// After the end of the main thread, the spawned thread will be terminated.
